024f7ea48d30992aee13e48e7c9c7b7125450776
andremoore123 <Andremoore431@gmail.com>
2022-02-12 09:29:22 +0700

    Done V1

--- app/src/main/java/com/andre/bmicalculator/HitungActivity.kt
+++ app/src/main/java/com/andre/bmicalculator/HitungActivity.kt
@@ -1,11 +1,30 @@
 package com.andre.bmicalculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class HitungActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hitung)
+
+        val button_hitung_ulang = findViewById<Button>(R.id.button_hitung_ulang)
+        val bmi_text = findViewById<TextView>(R.id.skor_bmi)
+
+
+        val bmi = intent.getDoubleExtra(EXTRA_MESSAGE, 0.0)
+        val bmiScore = if(bmi == 0.0) "Inputan tidak valid" else bmi.toString()
+        bmi_text.text = bmiScore
+        button_hitung_ulang.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
+
+
 }
--- app/src/main/java/com/andre/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/andre/bmicalculator/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.andre.bmicalculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.myriads.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,10 +14,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val button_hitung = findViewById<Button>(R.id.button_hitung)
+        val tinggi = findViewById<EditText>(R.id.input_tinggi).text
+        val berat = findViewById<EditText>(R.id.input_berat).text
 
         button_hitung.setOnClickListener {
-
+            val beratDouble = if(berat.isNotEmpty()) berat.toString().toDouble() else 0.0
+            val tinggiDouble = if(tinggi.isNotEmpty()) tinggi.toString().toDouble()/100 else 0.0
+            val bmi = beratDouble / (tinggiDouble * tinggiDouble)
+            val intent = Intent(this, HitungActivity::class.java).apply {
+                putExtra(EXTRA_MESSAGE, bmi)
+            }
+            startActivity(intent)
+            finish()
         }
     }
-
 }
