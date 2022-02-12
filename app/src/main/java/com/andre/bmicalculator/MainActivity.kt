a58b98dbffb7b1ab037bc72abe0ec226e4f64c30
andremoore123 <Andremoore431@gmail.com>
2022-02-12 07:01:31 +0700

    Add Activity Hitung1

--- app/src/main/java/com/andre/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/andre/bmicalculator/MainActivity.kt
@@ -2,10 +2,21 @@ package com.andre.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button_hitung = findViewById<Button>(R.id.button_hitung)
+
+        button_hitung.setOnClickListener {
+
+        }
     }
+
 }
